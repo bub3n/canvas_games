bfc3d9e0d5d89a65e0a66445f4c39207eba06381
bub3n <43008808+bub3n@users.noreply.github.com>
2024-04-19 22:48:11 +0200

    Update index.js

--- game_1/index.js
+++ game_1/index.js
@@ -25,25 +25,33 @@ class Sprite {
     this.color = color
   }
 
+  // draw characer function
   draw() {
     c.fillStyle = this.color
     c.fillRect(this.position.x, this.position.y, 50, this.height)
   }
 
+  // update positions
   update() {
+    // draw rectangle
     this.draw()
+    // move rectangle
     this.position.y += this.velocity.y
 
     if ( jump == true ) {
+        // go up if still in jump
         if ( this.position.y - this.velocity.y >= jump_end_player ) {
             console.log(this.position.y)
             this.velocity.y -= gravity
+        // disable jump on and of jump
         } else {
             jump = false
         }
     } else {
+        // fall on ground
         if ( this.position.y + this.height + this.velocity.y >= canvas.height ){
             this.velocity.y = 0
+        // stop on ground
         } else {
             this.velocity.y += gravity
         }
@@ -65,8 +73,6 @@ const player = new Sprite({
 })
 // debug player object
 console.log(player)
-// call Sprite function in object player
-//player.draw()
 
 // create enemy object
 const enemy = new Sprite({
@@ -80,15 +86,20 @@ const enemy = new Sprite({
     },
     color: 'blue'
 })
+// debug enemy object
 console.log(enemy)
-//enemy.draw()
+
 
 
 // function that is called on every request on animation
 function animate() {
+    // do llop of itself
     window.requestAnimationFrame(animate)
+    // fill back with black
     c.fillStyle = 'black'
+    // clean background on every call
     c.fillRect(0, 0, canvas.width, canvas.height)
+    // reanimate characters
     player.update()
     enemy.update()
 }
@@ -96,6 +107,8 @@ function animate() {
 // something like main for animation
 animate()
 
+
+// add handler for listening on key press
 window.addEventListener('keydown', (event) => {
     console.log(event.key);
     if (event.key == " "){
