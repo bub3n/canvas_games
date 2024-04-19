91b961754ef06a74dbb69f972e1ec0a929f93fe0
bub3n <43008808+bub3n@users.noreply.github.com>
2024-04-19 22:40:04 +0200

    Update index.js

--- game_1/index.js
+++ game_1/index.js
@@ -8,32 +8,100 @@ canvas.height = 575
 // fill by rectangle
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+var jump = false
+var jump_enemy = false
+var jump_player = false
+var jump_start_enemy = 0
+var jump_start_player = 0
+const jump_height = 100
+
+const gravity = 0.2
 // method for defining sprite object
 class Sprite {
-  constructor(position, color) {
+  constructor({ position, velocity, color }) {
     this.position = position
+    this.velocity = velocity
+    this.height = 150
+    this.color = color
   }
 
   draw() {
     c.fillStyle = this.color
-    c.fillRect(this.position.x, this.position.y, 50, 150)
+    c.fillRect(this.position.x, this.position.y, 50, this.height)
+  }
+
+  update() {
+    this.draw()
+    this.position.y += this.velocity.y
+
+    if ( jump == true ) {
+        if ( this.position.y - this.velocity.y >= jump_end_player ) {
+            console.log(this.position.y)
+            this.velocity.y -= gravity
+        } else {
+            jump = false
+        }
+    } else {
+        if ( this.position.y + this.height + this.velocity.y >= canvas.height ){
+            this.velocity.y = 0
+        } else {
+            this.velocity.y += gravity
+        }
+    }
   }
 }
 
 // create object player
 const player = new Sprite({
-    x: canvas.width/2,
-    y: canvas.height/2,
-    color: red
+    position: {
+        x: canvas.width/2,
+        y: canvas.height/2
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    color: 'red'
 })
+// debug player object
+console.log(player)
+// call Sprite function in object player
+//player.draw()
+
 // create enemy object
 const enemy = new Sprite({
-    x: 0,
-    y: 0,
-    color: blue
+    position: {
+        x: 30,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    color: 'blue'
 })
+console.log(enemy)
+//enemy.draw()
 
 
+// function that is called on every request on animation
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
 
+// something like main for animation
+animate()
 
-console.log(player)
+window.addEventListener('keydown', (event) => {
+    console.log(event.key);
+    if (event.key == " "){
+        console.log("OH yeeeeeee")
+        jump = true
+        jump_end_enemy = enemy.position.y - jump_height
+        jump_end_player = player.position.y - jump_height
+    }
+})
